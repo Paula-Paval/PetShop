fc110e91d92411e53807bc441550e56597591dcf
Paula Paval <paval.paula2000@gmail.com>
2022-05-10 20:08:43 +0300

    refactor

--- solutie/PetShop/Commons/IModel.cs
+++ solutie/PetShop/Commons/IModel.cs
@@ -8,27 +8,22 @@ namespace Commons
 {
     public interface IModel
     {
-        
-
-
-
-
         void AddAnimal(Animal produs);
         void AddObiecteIngrijire(ObiectIngrijire produs);
         void AddJucarii(Jucarie produs);
         void AddHrana(Hrana produs);
 
-        bool  DeleteAnimal(int id);
+        bool DeleteAnimal(int id);
         bool DeleteObiectIngrijire(int id);
         bool DeleteJucarii(int id);
         bool DeleteHrana(int id);
 
         string ListAll(string tip);
-        void  Cumpara(string tip, string denumire);
-     
-        bool Exists(string  tip, string denumire);
-        void InitializeData();     
-       
-      
+        void Cumpara(string tip, string denumire);
+
+        bool Exists(string tip, string denumire);
+        void InitializeData();
+
+
     }
 }
